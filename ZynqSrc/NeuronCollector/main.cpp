079649b53d04ff4d86f717000436d657f6320946
Kim Bjerge <au288681@uni.au.dk>
2018-12-07 14:07:43 +0100

    Updated main to use loading test data from SD card

--- ZynqSrc/NeuronCollector/main.cpp
+++ ZynqSrc/NeuronCollector/main.cpp
@@ -15,9 +15,14 @@
 #include "DataUDPThread.h"
 #include "CliTCPThread.h"
 #include "UserThread.h"
+#include "TestDataGenerator.h"
+#include "TestDataSDCard.h"
 
 NeuronChannels neuronChannels;
-DataUDPThread dataThread;
+TestDataSDCard testDataSDCard;
+TestDataGenerator testDataGenerator;
+//DataUDPThread dataThread(&testDataGenerator);
+DataUDPThread dataThread(&testDataSDCard);
 CliCommand    cliCommand(&neuronChannels, &dataThread);
 CliTCPThread  cliThread;
 
@@ -27,6 +32,8 @@ int main()
 	//init_net_server(demo_udp_thread, 0);
 	//init_net_server(dataThread.threadMapper, &dataThread);
 
+	printf("Reading test samples from file: DATA.bin\r\n");
+	if (testDataSDCard.readFile((char *)"DATA.bin") != XST_SUCCESS) return 0;
 	cliThread.addCommand(&cliCommand);
 	init_net_server(cliThread.threadMapper, &cliThread);
 
