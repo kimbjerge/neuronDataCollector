34cb7cdfe0427724c716f6cb5cbe8a4abb517b28
Kim Bjerge <au288681@uni.au.dk>
2018-12-22 12:48:31 +0100

    Updated HLS NXCOR

--- HLS/NXCOR/NXCOR.h
+++ HLS/NXCOR/NXCOR.h
@@ -1,27 +1,30 @@
 ///////////////////////////////////////////////////////////
 //  NXCOR.h
-//  Header:          Normalized Cross Correlation functions.
-//  Created on:      23-10-2017
-//  Original author: MB
+//  Header:          Normalized Cross Correlation function
+//  Created on:      20-12-2018
+//  Original author: Kim Bjerge
 ///////////////////////////////////////////////////////////
-//#ifndef NXCOR_H
-//#define NXCOR_H
-#define SC_INCLUDE_FX
+#ifndef NXCOR_H
+#define NXCOR_H
+
+#include "stdint.h"
+
+//#define SC_INCLUDE_FX
 //#define SC_FX_EXCLUDE_OTHER
-#include <systemc.h>
-
-#define 				T									float
-//#define 				T									sc_fixed<32,8,SC_RND_ZERO,SC_SAT>
+//#include <systemc.h>
+//#define 				T									sc_fixed<64,62,SC_RND_ZERO,SC_SAT>
+#define 				T									int64_t
+//#define 				T									float
 #define 				DATA_CHANNELS 						32
-#define					TEMPLATE_CROPPED_LENGTH				17
-#define					TEMPLATE_CROPPED_WIDTH				9
-#define                 SIGNAL_LENGTH						20
+#define					TEMPLATE_CROPPED_LENGTH				16
+#define					TEMPLATE_CROPPED_WIDTH				8
+#define                 INIT_VALUE                          0x00007fff
 
-/* NXCOR functions calls*/
-void runNXCOR(T result[SIGNAL_LENGTH], T input[SIGNAL_LENGTH*DATA_CHANNELS], T template_[TEMPLATE_CROPPED_LENGTH*TEMPLATE_CROPPED_WIDTH], uint32_t templateLength, uint32_t templateChannels, uint32_t signalLength,  uint32_t signalLowerIndex);
+void initNXCOR(void);
 
-//void runNXCOR_STD(T* result, T* signal, T* template_, uint32_t templateLength, uint32_t templateChannels, uint32_t signalLength, uint32_t signalLowerIndex, uint32_t numberOfChannelDrift);
-/* Helper Functions */
-//float performXTestReturnExecutionTime(T* result, T* signal, T* template_, uint32_t templateLength, uint32_t templateChannels, uint32_t signalLength, bool useOpenCV, uint32_t numberOfTest);
+/* NXCOR function call */
+void NXCOR(T *result, T *varSig, int32_t inputSignal[TEMPLATE_CROPPED_WIDTH],
+		   int32_t templateData[TEMPLATE_CROPPED_WIDTH*TEMPLATE_CROPPED_LENGTH],
+		   int32_t avgTemp);
 
-//#endif
+#endif
--- HLS/NXCOR/main.cpp
+++ HLS/NXCOR/main.cpp
@@ -7,15 +7,69 @@
 #ifndef NXCOR_CONTROLLER_H
 #define NXCOR_CONTROLLER_H
 
-//#include "stdint.h"
-//#include "math.h"
+#include "stdio.h"
 #include "NXCOR.h"
+#include "math.h"
+
+T meanTemp(int32_t* inputPatch, uint32_t r, uint32_t c, uint32_t stride) //, uint32_t wt, uint32_t ht)
+{
+	T average = 0;
+	mean_label0:for (uint32_t i = r; i<TEMPLATE_CROPPED_LENGTH + r; i++)
+		for (uint32_t j = c; j<TEMPLATE_CROPPED_WIDTH + c; j++) {
+			average += inputPatch[(i * stride) + j]; // Computes average
+		}
+	average = average / (TEMPLATE_CROPPED_WIDTH*TEMPLATE_CROPPED_LENGTH);
+	return average;
+}
 
 int main() {
-	T result[SIGNAL_LENGTH];
-	T signal[SIGNAL_LENGTH*DATA_CHANNELS];
-	T tempA[TEMPLATE_CROPPED_LENGTH*TEMPLATE_CROPPED_WIDTH];
-	runNXCOR(result, signal, tempA, TEMPLATE_CROPPED_LENGTH, TEMPLATE_CROPPED_WIDTH, SIGNAL_LENGTH, 1);
+	T result;
+	int32_t signalTest[DATA_CHANNELS];
+	int32_t tempA[TEMPLATE_CROPPED_LENGTH*TEMPLATE_CROPPED_WIDTH];
+	T avgTemp = 0;   // Average template
+	T varTemp = 0;   // Variance template
+	T varSignal = 0; // Variance signal
+
+	//initNXCOR();
+
+	// Set test values in signal
+	for (uint16_t j = 0; j < DATA_CHANNELS; j++) {
+		signalTest[j] = INIT_VALUE;
+	}
+	// Set test values in template
+	for (uint16_t i = 0; i < TEMPLATE_CROPPED_LENGTH; i++) // Cross correlation with template
+		for (uint16_t j = 0; j < TEMPLATE_CROPPED_WIDTH; j++) {
+			tempA[i * TEMPLATE_CROPPED_WIDTH + j] = INIT_VALUE;
+		}
+
+	// Computes average of template for red and blue color
+	avgTemp = meanTemp(tempA, 0, 0, TEMPLATE_CROPPED_WIDTH);
+
+	// Compute variance of template
+	for (uint16_t i = 0; i < TEMPLATE_CROPPED_LENGTH; i++) // Cross correlation with template
+		for (uint16_t j = 0; j < TEMPLATE_CROPPED_WIDTH; j++) {
+			T temp = tempA[i * TEMPLATE_CROPPED_WIDTH + j];
+
+			// Red color, cross correlation, variance pixel, variance template
+			T tr = temp - avgTemp;
+			varTemp = varTemp + (tr*tr);
+		}
+
+	for (uint16_t i = 0; i < TEMPLATE_CROPPED_LENGTH*2; i++)
+		NXCOR(&result, &varSignal, &signalTest[4], tempA, avgTemp);
+
+	float xcorr = result;
+	float varS = varSignal;
+	float varT = varTemp;
+	if (varS > 0 && varT > 0)
+		printf("Result NXCOR: %f\r\n", xcorr / sqrt(varS * varT));
+	else {
+		printf("Result XCOR: %f\r\n", xcorr);
+		printf("Result VarT: %f\r\n", varT);
+		printf("Result VarS: %f\r\n", varS);
+	}
+
+	return 0;
 }
 
 
