c25e965fc09c69e8dc6e23c10eac4b8cbacdace6
Kim Bjerge <au288681@uni.au.dk>
2019-01-06 14:22:45 +0100

    Corrected creating FIR coefficients

--- MATLAB/SaveFilterTxtFile.m
+++ MATLAB/SaveFilterTxtFile.m
@@ -2,11 +2,8 @@ function [ ] = SaveFilterTxtFile(  y, fileName  )
 % Save coefficeints to fileName
     fid = fopen(fileName, 'w');
     for i=1:length(y)
-        if (i < length(y))
-            fprintf(fid, '%1.15f\r\n', y(i));
-        end
+        fprintf(fid, '%1.15f\r\n', y(i));
     end
     fclose(fid);
-
 end
 
